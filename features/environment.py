0a930f7f96b95f0fb5134cc3be4c385f9bacd650
Ilarie Vilbrun <ilarievilbrun@Ilaries-Air.home>
2024-11-13 00:28:12 -0500

    task 2 commit

--- features/environment.py
+++ features/environment.py
@@ -10,13 +10,13 @@ def browser_init(context):
     """
     :param context: Behave context
     """
-    # driver_path = ChromeDriverManager().install()
-    # service = Service(driver_path)
-    # context.driver = webdriver.Chrome(service=service)
-
-    driver_path = GeckoDriverManager().install()
+    driver_path = ChromeDriverManager().install()
     service = Service(driver_path)
-    context.driver = webdriver.Firefox(service=service)
+    context.driver = webdriver.Chrome(service=service)
+
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
